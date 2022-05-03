152040a2cd96566d75bec7ca3e6c13f1945bbac9
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-05-03 16:50:47 +0200

    Mål tid og log

--- src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
@@ -1,10 +1,12 @@
 package no.nav.helse.flex.kafka
 
+import no.nav.helse.flex.logger
 import no.nav.helse.flex.paske.Paskemetrikk
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import kotlin.system.measureTimeMillis
 
 const val SENDT_SYKEPENGESOKNAD_TOPIC = "flex." + "syfosoknad-sykepengesoknad-migrering"
 
@@ -13,6 +15,8 @@ class SykepengesoknadListener(
     private val paskemetrikk: Paskemetrikk,
 ) {
 
+    val log = logger()
+
     @KafkaListener(
         topics = [SENDT_SYKEPENGESOKNAD_TOPIC],
         concurrency = "1",
@@ -20,10 +24,13 @@ class SykepengesoknadListener(
     )
     fun listenBatch(cr: List<ConsumerRecord<String, String>>, acknowledgment: Acknowledgment) {
 
-        cr.forEach {
-            paskemetrikk.prossesser(it.value())
+        log.info("Mottar ${cr.size} records")
+        val time = measureTimeMillis {
+            cr.forEach {
+                paskemetrikk.prossesser(it.value())
+            }
+            acknowledgment.acknowledge()
         }
-
-        acknowledgment.acknowledge()
+        log.info("Prossesserte ${cr.size} records iløpet av $time millis")
     }
 }
